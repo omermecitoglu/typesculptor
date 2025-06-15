226d4f0ae84dd7bb1c4efb1b53f3192a43247e42
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-06-15 09:05:56 +0700

    chore: add initial package info

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { exampleFunction } from "./index";
-
-describe("exampleFunction", () => {
-  it("should be a function", () => {
-    expect(typeof exampleFunction).toBe("function");
-  });
-});
